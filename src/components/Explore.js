29c4486317b109389f8a594a4527f720efc8515b
hcoz <hcanozcelik@gmail.com>
2021-02-26 14:48:41 +0300

    App: select example added

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -2,12 +2,19 @@ import React, { useContext, useState } from 'react';
 
 import { Text, LanguageContext } from '../containers/Language';
 
+const selectOptions = ['option1', 'option2', 'option3'];
+
 export default function Explore() {
   const [clickText, setClickText] = useState();
+  const [selectedOption, setSelectedOption] = useState();
   const { dictionary } = useContext(LanguageContext);
 
   const handleClick = () => {
     setClickText(<Text tid="buttonClicked" />);
+  }
+
+  const handleOptionChange = e => {
+    setSelectedOption(e.target.value);
   }
 
   return (
@@ -21,6 +28,19 @@ export default function Explore() {
           <Text tid="clickMe" />
         </button>
         <p>{clickText}</p>
+      </div>
+
+      <div>
+        <select
+          onChange={handleOptionChange}
+          value={selectedOption}
+        >
+          {selectOptions.map(option => (
+            <option key={option} value={option}>
+              {dictionary[option]}
+            </option>
+          ))}
+        </select>
       </div>
 
       <a href="https://halilcanozcelik.com" target="_blank" rel="noopener noreferrer">
